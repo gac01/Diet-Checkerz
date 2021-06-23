0ee72cd6e1b6cff37a369e8f94df839be32bcc62
gac01 <gedearya.c@u.nus.edu>
2021-06-23 18:44:55 +0800

    Merge branch 'main' of https://github.com/gac01/Diet-Checkerz

--- diet-checkerz/src/index.js
+++ diet-checkerz/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { StrictMode } from "react";
 import ReactDOM from "react-dom";
-
+import "./reportWebVitals.js";
 import WelcomePage from "./Pages/welcome-page";
 import "./index.css";
 
@@ -12,7 +12,20 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
+const reportWebVitals = (onPerfEntry) => {
+  if (onPerfEntry && onPerfEntry instanceof Function) {
+    import("web-vitals").then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+      getCLS(onPerfEntry);
+      getFID(onPerfEntry);
+      getFCP(onPerfEntry);
+      getLCP(onPerfEntry);
+      getTTFB(onPerfEntry);
+    });
+  }
+};
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
