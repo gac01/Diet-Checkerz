f93524e63dee00940d0fbcc9a54d6615bd979b2f
LouisLouis19 <e0661385@u.nus.edu>
2021-06-25 18:02:29 +0800

    Login + Authentication with Firebase

--- diet-checkerz/src/App.js
+++ diet-checkerz/src/App.js
@@ -4,15 +4,43 @@ import Home from './Components/home';
 import AboutUs from './Components/about-us';
 import { makeStyles } from "@material-ui/core/styles";
 import './App.css';
+import fire from "../Config/fire";
+import MainPage from "../Pages/main-page";
+import WelcomePage from './Pages/welcome-page';
+import Login from './Components/login';
 
-function App() {
-  return (
-    <div className="App">
-      <NavBar />
-      <Home />
-      <AboutUs />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state={
+      user : {}
+    }
+  }
+  
+  componentDidMount() {
+    this.authListener();
+  }
+
+  authListener() {
+    fire.auth().onAuthStateChanged((user)=>{
+      if(user) {
+        this.setState({user})
+      } else{
+        this.setState({user : null})
+      }
+    })
+  }
+
+  render() {
+    return (
+      <div className="Login">
+        {this.state.user ? (<WelcomePage/>) : (<Login/>)}
+      </div>  
+    );
+  }
 }
 
 export default App;
+
+
+
--- diet-checkerz/src/Components/login.js
+++ diet-checkerz/src/Components/login.js
@@ -1,17 +1,70 @@
 import React from "react";
+import fire from "../Config/fire";
 import "../Stylesheet/login.module.css";
-//import css file for styling
 
-function Login() {
-  return (
-    <div class="login">
-      <form class="login-form">
-        <input type="text" placeholder="username"/>
-        <input type="password" placeholder="password"/>
-        <button>login</button>
-        </form>
-    </div>
-  );
+class Login extends Component{
+  constructor(props){
+    super(props);
+    this.login = this.login.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.signup = this.signup.bind(this);
+    this.state={
+      email : "",
+      password : ""
+    }
+  }
+
+  login(e){
+    e.preventDefault();
+    fire.auth().signInWithEmailAndPassword(this.state.email, this.state.password).then((u)=>{
+      console.log(u)
+    }).catch((err)=>{
+      console.log(err);
+    })
+  }
+
+  signup(e){
+    e.preventDefault();
+    fire.auth().createUserWithEmailAndPassword(this.state.email, this.state.password).then((u)=> {
+      console.log(u)
+    }).catch((err)=>{
+      console.log(err);
+    })
+  }
+
+  handleChange(e){
+    this.setState({
+      [e.target.name] : e.target.value
+    })
+  }
+
+  render(){
+    return (
+      <div class="login">
+        <form class="login-form">
+          <input 
+          name="email"
+          type="email" 
+          id="email"
+          placeholder="enter email address"
+          onChange={this.handleChange}
+          value={this.state.email}
+          />
+
+          <input 
+          name="password"
+          type="password"
+          id="password" 
+          placeholder="enter password"
+          onChange={this.handleChange}
+          value={this.state.password}
+          />
+          <button onCLick={this.login}>Login</button>
+          <button onCLick={this.signup}>Sign Up</button>
+          </form>
+      </div>
+    );
+  }
 }
 
 export default Login;
