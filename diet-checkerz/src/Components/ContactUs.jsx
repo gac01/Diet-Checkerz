13b8dfccc42ad674176f8f90098402ebab7de249
LouisLouis19 <69447277+LouisLouis19@users.noreply.github.com>
2021-07-26 23:12:35 +0800

    Update ContactUs.jsx

--- diet-checkerz/src/Components/ContactUs.jsx
+++ diet-checkerz/src/Components/ContactUs.jsx
@@ -1,11 +1,25 @@
 import React from "react";
- 
+import "../Stylesheet/contactUs.module.css";
+
 function ContactUs() {
-    return (
-        <div>
-            Hello World!
-        </div>
-      );
+  return (
+    <div>
+      <h1>About Diet-Checkerz</h1>
+      <h6>
+        Diet-Checkerz is a web application project created during Orbital 2021
+        <br />
+        Created by Louis (Y1, Information Security) and Gede Arya Chandradhana
+        (Y2, Chemical Engineering)
+        <br />
+        <br />
+        The creation of Diet-Checkerz is inspired by our personal health journey
+        where our diets failed us.
+        <br />
+        We hope that this web application can be used to help people like us
+        stay more disciplined in our diets and training.
+      </h6>
+    </div>
+  );
 }
- 
+
 export default ContactUs;
