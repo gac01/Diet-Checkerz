38cf948b5ac4bb30da752dca834da57ccfedb954
gac01 <83939939+gac01@users.noreply.github.com>
2021-07-26 15:42:14 +0800

    Update fire.js

--- diet-checkerz/src/utils/fire.js
+++ diet-checkerz/src/utils/fire.js
@@ -4,11 +4,13 @@ import "firebase/firestore";
 var firebaseConfig = {
   apiKey: "AIzaSyDe_qC4_xixxgA7mtP-62gCjiEmai9ZfJ4",
   authDomain: "diet-checkerz-login-auth.firebaseapp.com",
+  databaseURL:
+    "https://diet-checkerz-login-auth-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "diet-checkerz-login-auth",
   storageBucket: "diet-checkerz-login-auth.appspot.com",
   messagingSenderId: "172319731850",
-  appId: "1:172319731850:web:8e7d478c729a1fce2b3d73",
-  measurementId: "G-6NYQQTHTP4",
+  appId: "1:172319731850:web:ca6a474861f97ab52b3d73",
+  measurementId: "G-E82F86NJLG",
 };
 
 export const fire = firebase.initializeApp(firebaseConfig);
